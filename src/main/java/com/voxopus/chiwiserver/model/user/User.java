9991e4c3b66115f128c9bda91cd33e157c6132a9
RedFlameKen <kabalacaba@gmail.com>
2025-10-24 16:53:41 +0800

    feat(user): add service function for creating a user

--- src/main/java/com/voxopus/chiwiserver/model/user/User.java
+++ src/main/java/com/voxopus/chiwiserver/model/user/User.java
@@ -12,12 +12,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Table(name="user")
 public class User {
 
--- src/main/java/com/voxopus/chiwiserver/service/user/UserService.java
+++ src/main/java/com/voxopus/chiwiserver/service/user/UserService.java
@@ -1,11 +1,27 @@
 package com.voxopus.chiwiserver.service.user;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.voxopus.chiwiserver.model.user.User;
+import com.voxopus.chiwiserver.repository.user.UserRepository;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     public void createUser(String username, String password, String saltIv){
+        User user = User.builder()
+            .username(username)
+            .password(password)
+            .salt_iv(saltIv)
+            .date_created(new Date())
+            .build();
+        userRepository.save(user);
     }
     
 }
