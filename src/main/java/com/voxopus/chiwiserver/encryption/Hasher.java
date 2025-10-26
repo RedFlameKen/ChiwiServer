1535a706da1f61b3a61dfb27a8fd1d154b460146
RedFlameKen <kabalacaba@gmail.com>
2025-10-26 12:59:27 +0800

    feat: create a hasher util

---
+++ src/main/java/com/voxopus/chiwiserver/encryption/Hasher.java
@@ -0,0 +1,48 @@
+package com.voxopus.chiwiserver.encryption;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class Hasher {
+
+    private static final String KEY_ALGO = "PBKDF2WithHmacSHA1";
+    private static final int KEY_SPEC_ITER = 65536;
+    private static final int KEY_SPEC_LEN = 128;
+
+    private byte[] salt;
+
+    public Hasher(){
+        this.salt = generateBytes(16);
+    }
+
+    public String hash(String data){
+        byte[] hash = null;
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance(KEY_ALGO);
+            KeySpec spec = new PBEKeySpec(data.toCharArray(), salt, 
+                    KEY_SPEC_ITER, KEY_SPEC_LEN);
+            hash = factory.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return Encoder.encodeBase64(hash);
+    }
+
+    public String getSalt() {
+        return Encoder.encodeBase64(salt);
+    }
+
+    public byte[] generateBytes(int range){
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[range];
+        random.nextBytes(bytes);
+        return bytes;
+    }
+    
+}
