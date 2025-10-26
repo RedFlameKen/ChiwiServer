18c59ace0ae458a4f3fbd0205f789b40b37d703e
RedFlameKen <kabalacaba@gmail.com>
2025-10-26 12:22:13 +0800

    feat: add user repository query for finding by username

--- src/main/java/com/voxopus/chiwiserver/repository/user/UserRepository.java
+++ src/main/java/com/voxopus/chiwiserver/repository/user/UserRepository.java
@@ -1,6 +1,7 @@
 package com.voxopus.chiwiserver.repository.user;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,7 @@ import com.voxopus.chiwiserver.model.user.User;
 public interface UserRepository extends JpaRepository<User, Long>{
 
     List<User> findByUsernameContainingIgnoreCase(String keyword);
+
+    Optional<User> findByUsername(String username);
 
 }
