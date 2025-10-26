2565ca14fde2d1403cde7c4b92fe7b15e5756de0
RedFlameKen <kabalacaba@gmail.com>
2025-10-26 12:23:03 +0800

    feat: add signup functionality

--- src/main/java/com/voxopus/chiwiserver/controller/user/UserController.java
+++ src/main/java/com/voxopus/chiwiserver/controller/user/UserController.java
@@ -1,9 +1,51 @@
 package com.voxopus.chiwiserver.controller.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.voxopus.chiwiserver.model.user.User;
+import com.voxopus.chiwiserver.request.user.UserRequestData;
+import com.voxopus.chiwiserver.response.ResponseData;
+import com.voxopus.chiwiserver.service.user.UserService;
+import com.voxopus.chiwiserver.util.Checker;
 
 @RestController
 public class UserController {
 
-    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody UserRequestData body){
+        Checker<User> checker = userService.createUser(
+                body.getUsername(), 
+                body.getPassword(), 
+                body.getSalt_iv());
+
+        HttpStatus status;
+        ResponseData<?> response;
+
+        if(!checker.isOk()){
+            status = HttpStatus.CONFLICT;
+            response = ResponseData.builder()
+                .statusCode(status.value())
+                .message("username is unavailable")
+                .build();
+
+            return new ResponseEntity<>(response, status);
+        }
+
+        status = HttpStatus.OK;
+        response = ResponseData.builder()
+            .statusCode(status.value())
+            .message("successfully registered the account")
+            .build();
+
+        return new ResponseEntity<>(response, status);
+    }
+
 }
--- src/main/java/com/voxopus/chiwiserver/service/user/UserService.java
+++ src/main/java/com/voxopus/chiwiserver/service/user/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.voxopus.chiwiserver.model.user.User;
 import com.voxopus.chiwiserver.repository.user.UserRepository;
+import com.voxopus.chiwiserver.util.Checker;
 
 @Service
 public class UserService {
@@ -14,7 +15,12 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public void createUser(String username, String password, String saltIv){
+
+    public Checker<User> createUser(String username, String password, String saltIv){
+        if(usernameUsed(username)){
+            return Checker.fail("username is unnavailable");
+        }
+
         User user = User.builder()
             .username(username)
             .password(password)
@@ -22,6 +28,12 @@ public class UserService {
             .date_created(new Date())
             .build();
         userRepository.save(user);
+
+        return new Checker<>(user);
     }
-    
+
+    private boolean usernameUsed(String username){
+        return userRepository.findByUsername(username).isPresent();
+    }
+
 }
