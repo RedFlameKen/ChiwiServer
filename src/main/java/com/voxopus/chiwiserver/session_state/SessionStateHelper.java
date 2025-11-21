e99150d152eea111c1941daa987c6730da50b79d
RedFlameKen <nettolacaba@gmail.com>
2025-11-21 23:42:56 +0800

    fix: add yep to checkConfirm

--- src/main/java/com/voxopus/chiwiserver/session_state/SessionStateHelper.java
+++ src/main/java/com/voxopus/chiwiserver/session_state/SessionStateHelper.java
@@ -8,6 +8,7 @@ public class SessionStateHelper {
         switch (confirm) {
             case "yes":
             case "yup":
+            case "yep":
             case "yuh":
             case "yeah":
                 return ConfirmEnum.YES;
